55aeaccf5a5827099798804fa7d6421e861dcb8d
RomanPrus <skimunion@gmail.com>
2022-11-03 15:30:24 +0200

    hw3

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const ulEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${ulEl.length}`);
+
+ulEl.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
