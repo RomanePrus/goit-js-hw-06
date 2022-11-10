5f0808baf2194b9648d3342fcb4f563644695cc4
RomanPrus <skimunion@gmail.com>
2022-11-10 09:14:10 +0200

    hw9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  btnEl: document.querySelector("button.change-color"),
+  bodyEl: document.querySelector("body"),
+  btnColorValue: document.querySelector("span.color"),
+};
+
+refs.btnEl.addEventListener("click", onBtnClick);
+
+function onBtnClick(event) {
+  let randomColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = randomColor;
+  refs.btnColorValue.textContent = randomColor;
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector("input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+refs.inputEl.addEventListener("input", e);
+// console.log(refs.inputEl);
+// const amount = refs.inputEl.value;
+function e() {
+  const inputValue = refs.inputEl.value;
+  console.log(inputValue);
+}
+// for (let i = 0; i < amount; i += 1) {
+//   const element = array[i];
+// }
